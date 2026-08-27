08ba13bcf56a0d0203bb60e25137bb7cb9202385
Nikolay <air19twist90@gmail.com>
2026-08-27 05:39:24 -0700

    Publish Nik'Os 2026-08-27

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
    online; offline still works because every response is cached on the way
    through. BUILD is stamped at publish time, which also purges old caches. */
 
-const BUILD = "20260827-053533";
+const BUILD = "20260827-053923";
 const CACHE = `nikos-shell-${BUILD}`;
 
 const APP_SHELL = [
